90617f339731d3a817d43077d1e0f01561aceb9a
Mark <mark-r@live.nl>
2014-05-19 14:32:58 +0200

    Code uitgelijnd en meer valide gemaakt (omgezet naar HTML5)

--- contact.php
+++ contact.php
@@ -1,49 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Mobiliteit Noord Groningen</title>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<script src="lib/jquery.min.js"></script>
-	<script src="lib/toegankelijkheid.js"></script>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Mobiliteit Noord Groningen</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <script src="lib/jquery.min.js"></script>
+        <script src="lib/toegankelijkheid.js"></script>
+    </head>
+    <body>
+        <div id="container">
+            <div id="menu" class="menuPlanNormal"> <img src="images/contrast.png"  alt="Wijzig contrast"/>
+                <div id="textSize">
+                    <a href="#" class="fontSizePlus">A+</a> | <a href="#" class="fontSizeMinus">A-</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="index.htm">Home</a>
+                    </li>
+                    <li>Waarom OV</li>
+                    <li>
+                        <a href="contact.php" >Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="slogan" class="footerSloganNormal">Reis met het openbaar vervoer!</div>
+            <div id="header"></div>
+            <div id="plan" class="menuPlanNormal">Contact ons</div>
+            <div id="contact" class="textNormal">
+                <form>
 
-<body>
-	<div id="container">
-		<div id="menu" class="menuPlanNormal"> <img src="images/contrast.png"/>
-			<div id="textSize">
-				<a href="#" class="fontSizePlus">A+</a> | <a href="#" class="fontSizeMinus">A-</a>
-			</div>
-			<ul>
-				<li>
-					<a href="index.htm">Home</a>
-				</li>
-				<li>Waarom OV</li>
-				<li>
-					<a href="contact.php" >Contact</a>
-				</li>
-			</ul>
-		</div>
-		<div id="slogan" class="footerSloganNormal">Reis met het openbaar vervoer!</div>
-		<div id="header"></div>
-		<div id="plan" class="menuPlanNormal">Contact ons</div>
-		<div id="contact" class="textNormal">
-			<form>
-        
-			    <label>Name</label>
-			    <input name="name" placeholder="Type Here">
-			            
-			    <label>Email</label>
-			    <input name="email" type="email" placeholder="Type Here">
-			            
-			    <label>Message</label>
-			    <textarea name="message" placeholder="Type Here"></textarea>
-			            
-			    <input id="submit" name="submit" type="submit" value="Submit"/>
-			        
-			</form>
-		</div>
-		<div id="footer" class="footerSloganNormal">&copy; 2014 - by INF2D</div>
-	</div>
-</body>
+                    <label>Name</label>
+                    <input name="name" placeholder="Type Here">
+
+                    <label>Email</label>
+                    <input name="email" type="email" placeholder="Type Here">
+
+                    <label>Message</label>
+                    <textarea name="message" placeholder="Type Here"></textarea>
+
+                    <input id="submit" name="submit" type="submit" value="Submit"/>
+
+                </form>
+            </div>
+            <div id="footer" class="footerSloganNormal">&copy; 2014 - by INF2D</div>
+        </div>
+    </body>
 </html>
