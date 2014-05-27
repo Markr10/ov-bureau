3615e9f84b1f0479d3ea6a085c15843d5f6f246b
Danny Pieters <danny.pieters@student.stenden.com>
2014-05-27 12:57:19 +0200

    Tweede layout

---
+++ new_layout.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Mobiliteit Noord Groningen</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link rel="stylesheet" type="text/css" href="css/form-planner.css" />
+        <script src="lib/jquery.min.js"></script>
+        <script src="lib/toegankelijkheid.js"></script>
+        <script src="lib/jquery.clearsearch-1.0.3-patched.js"></script>
+        <script src="lib/planner.js"></script>
+    </head>
+    <body>
+        <div id="container">
+            <div id="menu" class="menuPlanNormal"> <img src="images/contrast.png" alt="Wijzig contrast"/>
+                <div id="textSize">
+                    <a href="#" class="fontSizePlus">A+</a> | <a href="#" class="fontSizeMinus">A-</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="waarom-ov.html">Waarom OV</a>
+                    </li>
+                    <li>
+                        <a href="contact.php" >Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="slogan" class="footerSloganNormal">Reis met het openbaar vervoer!</div>
+            <div id="header"></div>
+            <div id="plan" class="menuPlanNormal">Plan uw reis!</div>
+            <div id="content" class="textNormal">
+                <form id="planner" method="post" action="planner.php">
+                    <label for="from" title="Vul bijvoorbeeld een adres, station, postcode in.">Van</label>
+                    <input name="from" id="from" class="clearable" type="text" placeholder="Adres, station, postcode, etc" autofocus />
+                    <label for="to" title="Vul bijvoorbeeld een adres, station, postcode in.">Naar</label>
+                    <input name="to" id="to" class="clearable" type="text" placeholder="Adres, station, postcode, etc" autofocus />
+                    <input name="submit" type="submit" value="Plannen" />
+                </form>
+            </div>
+            <div id="footer" class="footerSloganNormal">&copy; 2014 - by INF2D</div>
+        </div>
+    </body>
+</html>
